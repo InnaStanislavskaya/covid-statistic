62d4123ae0fb4fe30ae6275404c575abd9fb8e4b
InnaStanislavskaya <stanislavskaya.i.b@gmail.com>
2021-12-09 18:52:40 +0200

    the functional part is ready

--- src/Loader/Loader.js
+++ src/Loader/Loader.js
@@ -1,4 +1,11 @@
 import React from 'react'
 import './Loader.css'
 
-export default () => <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
+
+function Loader () {
+    return (
+        <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
+    )
+}
+
+export default Loader
