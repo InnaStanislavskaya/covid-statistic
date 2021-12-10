68205e83921091110c0a4b959858af3351e2861a
InnaStanislavskaya <stanislavskaya.i.b@gmail.com>
2021-12-10 13:45:41 +0200

    add logo

--- src/DetailRowView/DetailRowView.js
+++ src/DetailRowView/DetailRowView.js
@@ -7,9 +7,9 @@ function DetailRowView (props){
         <div className="detailRowView">
         <div className="detailRowView_content">
             <h3>{props.country.Country}</h3>
-            <div>Total Confirmed <b>{props.country.TotalConfirmed}</b></div>
-            <div>Total Deaths <b>{props.country.TotalDeaths}</b></div>
-            <div>Total Recovered <b>{props.country.TotalRecovered}</b></div>
+            <div>Total Confirmed: <b>{props.country.TotalConfirmed}</b></div>
+            <div>Total Deaths: <b>{props.country.TotalDeaths}</b></div>
+            <div>Total Recovered: <b>{props.country.TotalRecovered}</b></div>
             <button onClick = {props.onOkPressed.bind(null)}>OK</button>
         </div>
     </div>
--- src/TableSearch/TableSearch.js
+++ src/TableSearch/TableSearch.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import './TableSearch.css'
+import logo from './images/logo.png'
 
 function TableSearch (props) {
 
@@ -11,20 +12,26 @@ function TableSearch (props) {
     }
 
     return (
-        <div className="input-group mb-3 mt-3">
-            <input 
-                type="text" 
-                className="form-control" 
-                placeholder="Search" 
-                onChange={valueChangeHandler}
-                value={value}
-            />
-            <div className="input-group-append">
-                <button 
-                    className="btn btn-outline-secondary" 
-                    type="button"
-                    onClick={() => props.onSearch(value)}
-                >Button</button>
+        <div className="header">
+            <div className="header_logo">
+                <img src={logo} alt="logo"/>
+                <div className="header_logo_text">STATISTIC</div>
+            </div>
+            <div className="input-group w-25 mb-3 mt-3 header_search">
+                <input 
+                    type="text" 
+                    className="form-control" 
+                    placeholder="Search" 
+                    onChange={valueChangeHandler}
+                    value={value}
+                />
+                <div className="input-group-append">
+                    <button 
+                        className="btn btn-outline-secondary" 
+                        type="button"
+                        onClick={() => props.onSearch(value)}
+                    >Button</button>
+                </div>
             </div>
         </div>
     )
