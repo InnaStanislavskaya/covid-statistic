9cf2748e964ff331e445b905a3c3daf399f6dd12
InnaStanislavskaya <stanislavskaya.i.b@gmail.com>
2021-12-10 18:10:44 +0200

    added styles for detailRowView

--- src/DetailRowView/DetailRowView.js
+++ src/DetailRowView/DetailRowView.js
@@ -7,9 +7,24 @@ function DetailRowView (props){
         <div className="detailRowView">
         <div className="detailRowView_content">
             <h3>{props.country.Country}</h3>
-            <div>Total Confirmed: <b>{props.country.TotalConfirmed}</b></div>
-            <div>Total Deaths: <b>{props.country.TotalDeaths}</b></div>
-            <div>Total Recovered: <b>{props.country.TotalRecovered}</b></div>
+            <div className="detailRowView_content_item">
+                <div className="detailRowView_text">
+                    <div className="detailRowView_icon"><i className="fas fa-heartbeat"></i></div>
+                Total Confirmed</div>
+                <div>{props.country.TotalConfirmed}</div>
+            </div>
+            <div className="detailRowView_content_item">
+                <div className="detailRowView_text">
+                    <div className="detailRowView_icon"><i className="fas fa-skull-crossbones"></i></div>
+                Total Deaths</div>
+                <div>{props.country.TotalDeaths}</div>
+            </div>
+            <div className="detailRowView_content_item">
+                <div className="detailRowView_text">
+                <div className="detailRowView_icon"><i className="fas fa-file-medical"></i></div>
+                Total Recovered</div>
+                <div>{props.country.TotalRecovered}</div>
+            </div>
             <button onClick = {props.onOkPressed.bind(null)}>OK</button>
         </div>
     </div>
